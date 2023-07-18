f0429d4b57c38cb88f95c9c0f6f38a4ddbd7b526
Syaamil Maulana <106651762+Syaamilmaulana256@users.noreply.github.com>
2023-07-18 14:41:12 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,17 @@
 self.addEventListener('notificationclick', function (event) {
-  if (event.action === 'action-yes') {
-    alert("Kamu Memilih \"Ya\"")
-    console.log("Terpilih \"Ya\"");
-  } else if (event.action === 'action-no') {
-    alert("Kamu Memilih \"Tidak\"")
-    console.log("Terpilih \"Tidak\"");
-  } else {
-    console.log("Notifikasi Di Klik");
-  }
   event.notification.close();
-});
 
-self.addEventListener('push', function (event) {
-  if (event.data) {
-    const data = event.data.json();
-    const options = {
-      body: data.body,
-      icon: 'path/ke/gambar/icon-notifikasi.png', // Ganti dengan path gambar ikon notifikasi
-      actions: [
-        { action: 'action-yes', title: 'Ya' },
-        { action: 'action-no', title: 'Tidak' }
-      ]
-    };
-    event.waitUntil(self.registration.showNotification(data.title, options));
+  if (event.action === 'action-yes') {
+    alert("Tombol \"Ya\" Diklik.");
+    console.log('"Ya" Button Clicked.')
+    // Lakukan tindakan sesuai dengan tombol "Ya" di sini
+  } else if (event.action === 'action-no') {
+     alert("Tombol \"Tidak\" Diklik.");
+    console.log('"Tidak" Button Clicked.')
+    // Lakukan tindakan sesuai dengan tombol "Tidak" di sini
+  } else {
+    alert("Notfikasi Diklik.");
+    console.log('"ONLY-notfication" clicked')
+    // Lakukan tindakan sesuai dengan notifikasi diklik di sini
   }
 });
