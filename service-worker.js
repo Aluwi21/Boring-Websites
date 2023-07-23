ca59f96654060134bbdf3c840d8d55c221e69b61
Syaamil Maulana <106651762+Syaamilmaulana256@users.noreply.github.com>
2023-07-23 13:56:33 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,26 @@
 self.addEventListener('notificationclick', function (event) {
-  event.notification.close();
+    event.notification.close();
 
-  if (event.action === 'action-1') {
-    console.log('Tombol ' + event.action + ' Diklik.');
-    event.waitUntil(clients.matchAll().then(clients => {
-      clients.forEach(client => {
-        client.postMessage({ action: 'tampilkan_teks', teks: 'Kamu Menekan Tombol"' + event.action + '"', judul: event.notification.title, isi: event.notification.body });
-      });
-    }));
-  } else if (event.action === 'action-2') {
-    console.log('Tombol ' + event.action + ' Diklik');
-    event.waitUntil(clients.matchAll().then(clients => {
-      clients.forEach(client => {
-        client.postMessage({ action: 'tampilkan_teks', teks: 'Kamu Menekan Tombol"' + event.action + '"', judul: event.notification.title, isi: event.notification.body });
-      });
-    }));
-  } else {
-    console.log('Hanya Notifikasi Saja Yang Diklik.');
-    event.waitUntil(clients.matchAll().then(clients => {
-      clients.forEach(client => {
-        client.postMessage({ action: 'tampilkan_teks', teks: 'Kamu Hanya Mengklik Notifikasi', judul: event.notification.title, isi: event.notification.body });
-      });
-    }));
-  }
+    if (event.action === 'action-1') {
+        console.log('Tombol ' + event.action + ' Diklik.');
+        event.waitUntil(clients.matchAll().then(clients => {
+            clients.forEach(client => {
+                client.postMessage({ action: 'tampilkan_teks', teks: 'Tombol "' + event.notification.actions[0].title + '"', judul: event.notification.title, isi: event.notification.body });
+            });
+        }));
+    } else if (event.action === 'action-2') {
+        console.log('Tombol ' + event.action + ' Diklik');
+        event.waitUntil(clients.matchAll().then(clients => {
+            clients.forEach(client => {
+                client.postMessage({ action: 'tampilkan_teks', teks: 'Tombol "' + event.notification.actions[1].title + '"', judul: event.notification.title, isi: event.notification.body });
+            });
+        }));
+    } else {
+        console.log('Hanya Notifikasi Saja Yang Diklik.');
+        event.waitUntil(clients.matchAll().then(clients => {
+            clients.forEach(client => {
+                client.postMessage({ action: 'tampilkan_teks', teks: 'Hanya Mengklik Notifikasi', judul: event.notification.title, isi: event.notification.body });
+            });
+        }));
+    }
 });
