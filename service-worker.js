f86017cdb87f143b6cee92dce144554c6841be44
Syaamil Maulana <106651762+Syaamilmaulana256@users.noreply.github.com>
2023-08-01 14:52:15 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,14 +2,14 @@ self.addEventListener('notificationclick', function (event) {
     event.notification.close();
 
     if (event.action === 'button-1') {
-        console.log('Tombol ' + event.notification.actions[0].title + ' Diklik. / Tombol ' + event.action + 'Diklik.');
+        console.log('Tombol ' + '"' + event.notification.actions[0].title + '"' + ' Diklik. / Tombol ' + event.action + ' Diklik.');
         event.waitUntil(clients.matchAll().then(clients => {
             clients.forEach(client => {
                 client.postMessage({ action: 'notification-data', buttonclicked: 'Tombol "' + event.notification.actions[0].title + '"', title: event.notification.title, desc: event.notification.body });
             });
         }));
     } else if (event.action === 'button-2') {
-        console.log('Tombol ' + event.notification.actions[1].title + ' Diklik. / Tombol ' + event.action + 'Diklik.');
+        console.log('Tombol ' + '"' + event.notification.actions[1].title + '"' + ' Diklik. / Tombol ' + event.action + ' Diklik.');
         event.waitUntil(clients.matchAll().then(clients => {
             clients.forEach(client => {
                 client.postMessage({ action: 'notification-data', buttonclicked: 'Tombol "' + event.notification.actions[0].title + '"', title: event.notification.title, desc: event.notification.body });
