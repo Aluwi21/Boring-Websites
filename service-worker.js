52d5fbda3fe0f2e8ea18543192fe9a18e4c6fea4
Syaamil Maulana <106651762+Syaamilmaulana256@users.noreply.github.com>
2023-08-01 14:34:30 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,14 +2,14 @@ self.addEventListener('notificationclick', function (event) {
     event.notification.close();
 
     if (event.action === 'action-1') {
-        console.log('Tombol ' + event.action + ' Diklik.');
+        console.log('Tombol ' + event.notification.actions[0].title + ' Diklik. / Tombol ' + event.action + 'Diklik.');
         event.waitUntil(clients.matchAll().then(clients => {
             clients.forEach(client => {
                 client.postMessage({ action: 'tampilkan_teks', teks: 'Tombol "' + event.notification.actions[0].title + '"', judul: event.notification.title, isi: event.notification.body });
             });
         }));
     } else if (event.action === 'action-2') {
-        console.log('Tombol ' + event.action + ' Diklik');
+        console.log('Tombol ' + event.notification.actions[1].title + ' Diklik. / Tombol ' + event.action + 'Diklik.');
         event.waitUntil(clients.matchAll().then(clients => {
             clients.forEach(client => {
                 client.postMessage({ action: 'tampilkan_teks', teks: 'Tombol "' + event.notification.actions[1].title + '"', judul: event.notification.title, isi: event.notification.body });
