01f29964464259d99f5ffdf9da3815b36035bb86
Syaamil Maulana <106651762+Syaamilmaulana256@users.noreply.github.com>
2023-08-01 14:44:49 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,25 @@
 self.addEventListener('notificationclick', function (event) {
     event.notification.close();
 
-    if (event.action === 'action-1') {
+    if (event.action === 'button-1') {
         console.log('Tombol ' + event.notification.actions[0].title + ' Diklik. / Tombol ' + event.action + 'Diklik.');
         event.waitUntil(clients.matchAll().then(clients => {
             clients.forEach(client => {
-                client.postMessage({ action: 'tampilkan_teks', teks: 'Tombol "' + event.notification.actions[0].title + '"', judul: event.notification.title, isi: event.notification.body });
+                client.postMessage({ action: 'notification-data', buttonclicked: 'Tombol "' + event.notification.actions[0].title + '"', title: event.notification.title, desc: event.notification.body });
             });
         }));
-    } else if (event.action === 'action-2') {
+    } else if (event.action === 'button-2') {
         console.log('Tombol ' + event.notification.actions[1].title + ' Diklik. / Tombol ' + event.action + 'Diklik.');
         event.waitUntil(clients.matchAll().then(clients => {
             clients.forEach(client => {
-                client.postMessage({ action: 'tampilkan_teks', teks: 'Tombol "' + event.notification.actions[1].title + '"', judul: event.notification.title, isi: event.notification.body });
+                client.postMessage({ action: 'notification-data', buttonclicked: 'Tombol "' + event.notification.actions[0].title + '"', title: event.notification.title, desc: event.notification.body });
             });
         }));
     } else {
         console.log('Hanya Notifikasi Saja Yang Diklik.');
         event.waitUntil(clients.matchAll().then(clients => {
             clients.forEach(client => {
-                client.postMessage({ action: 'tampilkan_teks', teks: 'Hanya Mengklik Notifikasi', judul: event.notification.title, isi: event.notification.body });
+                client.postMessage({ action: 'notification-data', buttonclicked: 'Hanya Mengklik Notifikasi', title: event.notification.title, desc: event.notification.body });
             });
         }));
     }
